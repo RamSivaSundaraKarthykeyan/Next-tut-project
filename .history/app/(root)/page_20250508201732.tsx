0305e2a306b18293ec759b042f8ee26a6413f27c
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-05-08 20:22:12 +0530

    enna nu theriyala onume work aaga madiku

---
+++ .history/app/(root)/page_20250508201732.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="uppercase bg-black px-6 py-3 font-extrabold font-work-sans text-white sm: text [-54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+        Home
+      </h1>
+    </>
+  );
+}
